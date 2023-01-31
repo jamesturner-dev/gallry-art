6216092ce5a5fe480fc9be2f087c1d05bdce871c
jamesturner-dev <james@jamesturner.dev>
2023-01-31 10:17:36 -0500

    oy - YOU BROKE THE LOOP!

--- frontend/src/components/galleries/GalleriesLoop.vue
+++ frontend/src/components/galleries/GalleriesLoop.vue
@@ -1,17 +1,13 @@
 <template>
-  <ul role="list" class="gallGrid sm:grid-cols-4 md:gap-x-4 lg:gap-x-6 lg:gap-y-10 xl:grid-cols-6">
+  <ul role="list" class="gallGrid sm:grid-cols-4 md:gap-x-4 lg:gap-x-6 lg:gap-y-10 xl:grid-cols-6 pb-10">
     <li v-for="gallery in galleries" :key="gallery._id">
       <a :href="gallery.url" class="galleryItem">
         <img class="galleryImg lg:h-24 lg:w-24" :src="gallery.img" :alt=gallery.name />
         <div class="galleryBlurb">
           <h3 class="titleGallry">
-            <a :href="gallery.url">
-              {{ gallery.name }}
-            </a>
+            <a :href="gallery.url"> {{ gallery.name }} </a>
           </h3>
-          <p class="tinyText description">
-            {{ gallery.description }}
-          </p>
+          <p class="tinyText description"> {{ gallery.description }} </p>
         </div>
       </a>
     </li>
